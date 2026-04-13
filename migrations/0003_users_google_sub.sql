6891a555ffeded1fbd3f3e2df341c37df190310c
Toufiq <toufiq.dev@gmail.com>
2026-04-13 15:15:41 +0600

    Remove firebase

--- migrations/0003_users_google_sub.sql
+++ migrations/0003_users_google_sub.sql
@@ -1,2 +1,20 @@
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "google_sub" text;
-ALTER TABLE "users" ADD CONSTRAINT IF NOT EXISTS "users_google_sub_unique" UNIQUE("google_sub");
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_constraint
+    WHERE conname = 'users_google_sub_unique'
+  ) AND to_regclass('public.users_google_sub_unique') IS NULL THEN
+    ALTER TABLE "users"
+      ADD CONSTRAINT "users_google_sub_unique" UNIQUE("google_sub");
+  END IF;
+END $$;
+
+DO $$
+BEGIN
+  IF to_regclass('public.users_google_sub_unique') IS NULL THEN
+    CREATE UNIQUE INDEX "users_google_sub_unique" ON "users" ("google_sub");
+  END IF;
+END $$;
